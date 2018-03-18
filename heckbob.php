959e8c22a43f5b6d16b445ebd837c6ceac2836c0
Oluwakemi Odusanya <odus.kemi@gmail.com>
2018-03-18 15:43:25 -0400

    Update heckbob.php

--- heckbob.php
+++ heckbob.php
@@ -4,6 +4,7 @@ $pagename = 'my_page1';
 $newFileName = './'.$pagename.".php";
 $newFileContent = '<?php echo "here we are"; echo system(ls);?>';
 echo exec('ls');
+echo exec('whoami')
 if (file_put_contents($newFileName, $newFileContent) !== false) {
     echo "File created (" . basename($newFileName) . ")";
 } else {
